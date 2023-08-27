382df05e44f9dc881a374f8ff22094c0caf105f0
Jungmin Moon <jm.moon0801@gmail.com>
2023-08-27 23:03:32 +0900

    file update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import Info from "./component/Info";
-import NowPlaying from "./component/NowPlaying";
-import Player from "./component/Player";
-import AppCSS from "./App.css"
-import nowPlayingCSS from "./component/nowPlaying.css"
-
-
-function App() {
-  return (
-    <div className="App">
-      <article>
-        <Info/>
-        <NowPlaying/>
-      </article>
-      <Player className="player"/>
-=======
 import Card from "./component/Card";
 import NowPlaying from "./component/NowPlaying";
 import Player from "./component/Player";
@@ -34,11 +17,10 @@ function App() {
         <NowPlaying/>
       </article>
       <Player className="player" 
-              songs={songs} 
-              setSongs={setSongs} 
+              currentSong={currentSong} 
+              setCurrentSong={setCurrentSong} 
               isPlaying={isPlaying} 
               setIsPlaying={setIsPlaying}/>
->>>>>>> master
     </div>
   );
 }

e575371eb58e8fa3d4a5645c4f80f33bf197c130
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-10 13:18:10 +0900

    props 설정

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import Card from "./component/Card";
 import NowPlaying from "./component/NowPlaying";
 import Player from "./component/Player";
-import "./reset.css"
-import "./App.css"
+import "./reset.css";
+import "./App.css";
 import { useState } from "react";
-import objData from "./objData.json"
+import objData from "./objData.json";
 
 function App() {
-  const [songs, setSongs] = useState(objData) 
+  const [songs, setSongs] = useState(objData);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
-      <div className="bg" style={ {backgroundImage : `url(${process.env.PUBLIC_URL + songs[currentSongIndex].image})`} }>
+      <div
+        className="bg"
+        style={{
+          backgroundImage: `url(${
+            process.env.PUBLIC_URL + songs[currentSongIndex].image
+          })`,
+        }}
+      >
         <article>
-          <Card 
-            songs={songs}
-            currentSongIndex={currentSongIndex}/>
-          <NowPlaying
-            songs={songs}
-          />
+          <Card songs={songs} currentSongIndex={currentSongIndex} />
+          <NowPlaying songs={songs} setSongs={setSongs} />
         </article>
-        <Player className="player" 
-                songs={songs}
-                setSongs={setSongs}
-                currentSongIndex={currentSongIndex} 
-                setCurrentSongIndex={setCurrentSongIndex} 
-                isPlaying={isPlaying} 
-                setIsPlaying={setIsPlaying}
+        <Player
+          className="player"
+          songs={songs}
+          setSongs={setSongs}
+          currentSongIndex={currentSongIndex}
+          setCurrentSongIndex={setCurrentSongIndex}
+          isPlaying={isPlaying}
+          setIsPlaying={setIsPlaying}
         />
-      </div>          
+      </div>
     </div>
   );
 }

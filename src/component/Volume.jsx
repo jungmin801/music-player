36e51b43920cfdc888192295589bd99a04d1b1cd
Jungmin Moon <jm.moon0801@gmail.com>
2023-11-12 17:22:32 +0900

    뮤트 기능 추가

--- src/component/Volume.jsx
+++ src/component/Volume.jsx
@@ -1,12 +1,34 @@
+import React, { useState } from "react";
 import styles from "./css/volume.module.css";
+import * as S from "./css/Button";
 
-function VolumeModal({ volume, setVolume }) {
+function Volume({ volume, setVolume }) {
+  const [isMuted, SetIsMuted] = useState(false);
+  const [prevVolume, setPrevVolume] = useState(volume);
   const volumeBackground = {
-    background: `linear-gradient(to right, #FF0060 ${volume * 100}%, #fff ${volume * 100}%)`,
+    background: `linear-gradient(to right, #FF0060 ${volume * 100}%, #fff ${
+      volume * 100
+    }%)`,
   };
 
+  // 볼륨 뮤트하기
+  const handleMute = () => {
+    if (volume !== 0) {
+      setPrevVolume(volume);
+      setVolume(0);
+    } else {
+      setVolume(prevVolume);
+    }
+    SetIsMuted(!isMuted);
+  };
+
+  console.log(isMuted);
+
   return (
-    <div className={styles.volumeSliderBg}>
+    <div className={styles.volumeContainer}>
+      <button type="button" onClick={handleMute}>
+        {isMuted ? <S.MuteIcon /> : <S.VolumeIcon />}
+      </button>
       <input
         type="range"
         className={styles.volumeSlider}
@@ -14,10 +36,10 @@ function VolumeModal({ volume, setVolume }) {
         max="100"
         value={volume * 100}
         onChange={(e) => setVolume(e.target.value / 100)}
-        style = {volumeBackground}
+        style={volumeBackground}
       />
     </div>
   );
 }
 
-export default VolumeModal;
+export default Volume;

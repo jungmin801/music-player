33a4a7c91eaa4daf9cbc9ba7fbc3299382a7b3ba
Jungmin Moon <jm.moon0801@gmail.com>
2023-12-19 22:05:51 +0900

    애니메이션 추가 및 스타일 변경

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -8,6 +8,9 @@ import {
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./css/Login.module.css";
+import noteImg from "../image/note.png";
+import googleImg from "../image/google.png";
+import githubImg from "../image/github.png";
 
 const Login = () => {
   const [userData, setUserData] = useState(null);
@@ -49,17 +52,25 @@ const Login = () => {
   return (
     <section className={styles.section}>
       <div className={styles.container}>
+        <img src={noteImg} alt="로고" />
         <h1>Music Player</h1>
         <p>원하는 음악 파일을 업로드하여 나만의 재생 목록을 만들어보세요!</p>
       </div>
 
       <div className={styles.btnContainer} onClick={handleLogin}>
-        <button type="button" name="google">
-          구글 계정으로 로그인
-        </button>
-        <button type="button" name="github">
-          깃허브 계정으로 로그인
-        </button>
+        <div>
+          <button type="button" name="google">
+            <img src={googleImg} alt="" />
+            구글 계정으로 로그인
+          </button>
+        </div>
+
+        <div>
+          <button type="button" name="github">
+            <img src={githubImg} alt="" />
+            깃허브 계정으로 로그인
+          </button>
+        </div>
       </div>
     </section>
   );

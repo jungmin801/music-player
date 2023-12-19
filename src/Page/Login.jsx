ce9c8bc989aea2efaef4f251521d205710d45bc6
Jungmin Moon <jm.moon0801@gmail.com>
2023-12-19 11:38:11 +0900

    로그인 버튼 이벤트 위임

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -37,6 +37,14 @@ const Login = () => {
       });
   };
 
+  const handleLogin = (event) => {
+    if (event.target.name === "google") {
+      handleGoogleLogin();
+    } else if (event.target.name === "github") {
+      handleGitHubLogin();
+    }
+  };
+
   console.log(userData);
   return (
     <section className={styles.section}>
@@ -45,12 +53,12 @@ const Login = () => {
         <p>원하는 음악 파일을 업로드하여 나만의 재생 목록을 만들어보세요!</p>
       </div>
 
-      <div className={styles.btnContainer}>
-        <button type="button" onClick={handleGoogleLogin}>
-          구글 로그인으로 시작하기
+      <div className={styles.btnContainer} onClick={handleLogin}>
+        <button type="button" name="google">
+          구글 계정으로 로그인
         </button>
-        <button type="button" onClick={handleGitHubLogin}>
-          깃허브 로그인으로 시작하기
+        <button type="button" name="github">
+          깃허브 계정으로 로그인
         </button>
       </div>
     </section>

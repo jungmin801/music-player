53279e6b8edcfbdefdf65eb421070b268cf0c0bd
Jungmin Moon <jm.moon0801@gmail.com>
2023-12-26 10:42:58 +0900

    버튼 ui 수정

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./css/modal.module.css";
 import { storage, db } from "../service/firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
@@ -8,6 +8,7 @@ import { SongItemsAtom } from "../atoms/atomList";
 import basicImg from "../image/basic.jpg";
 
 export const InputModal = ({ setShowInputModal }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const songs = useRecoilValue(SongItemsAtom);
   const inputRef = useRef(null);
   let playListTitle = "";
@@ -20,6 +21,7 @@ export const InputModal = ({ setShowInputModal }) => {
     event.preventDefault();
     playListTitle = inputRef.current.value;
     try {
+      setIsLoading(true);
       const playlistCollectionRef = doc(db, "playlists", playListTitle);
       const playList = [];
 
@@ -37,6 +39,7 @@ export const InputModal = ({ setShowInputModal }) => {
         playList.push(songData);
       }
       await setDoc(playlistCollectionRef, { playList });
+      setIsLoading(false);
       setShowInputModal(false);
     } catch (error) {
       console.error("Error:", error);
@@ -50,19 +53,27 @@ export const InputModal = ({ setShowInputModal }) => {
         onSubmit={(event) => handleSaveList(event)}
       >
         <h3>저장할 재생 목록의 이름을 입력해주세요.</h3>
-        <input type="text" ref={inputRef} />
-        <div>
-          <button
-            className={styles.confirmButton}
-            type="button"
-            onClick={() => {
-              setShowInputModal(false);
-            }}
-          >
-            취소
-          </button>
-          <button className={styles.confirmButton}>확인</button>
-        </div>
+        {!isLoading ? (
+          <>
+            <input type="text" ref={inputRef} />
+            <div className={styles.btnContainer}>
+              <button
+                className={styles.confirmButton}
+                type="button"
+                onClick={() => {
+                  setShowInputModal(false);
+                }}
+              >
+                취소
+              </button>
+              <button className={styles.confirmButton}>확인</button>
+            </div>
+          </>
+        ) : (
+          <p className={styles.loading}>
+            저장하고 있습니다. 잠시만 기다려주세요.
+          </p>
+        )}
       </form>
     </div>
   );
@@ -114,7 +125,7 @@ export const ListModal = ({ loadList, setShowListModal }) => {
   return (
     <div className={styles.modalBg}>
       <div className={styles.modalContainer}>
-        <h3>재생할 목록을 선택하세요.</h3>
+        <h3>재생할 목록을 더블클릭하여 선택하세요.</h3>
         <ul>
           {loadList?.map((title, index) => (
             <li key={index} onDoubleClick={(event) => handleLoadMusic(event)}>
